45619672c177633f6be50b8ab2a500218fb6af5e
kgwny <210300265+kgwny@users.noreply.github.com>
2025-07-18 23:37:22 +0900

    add nuxt content demo files

---
+++ pages/articles/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <article v-if="article">
+    <h1>{{ article.title }}</h1>
+    <ContentRenderer :value="article" />
+  </article>
+</template>
+
+<script setup lang="ts">
+const { params } = useRoute()
+const { data: article } = await useAsyncData(() =>
+  queryContent(`/articles/${params.slug}`).findOne()
+)
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>記事一覧</h1>
+    <pre>{{ articles }}</pre> <!-- ← debug表示 -->
+    <ul>
+      <li v-for="article in articles" :key="article._path">
+        <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: articles } = await useAsyncData('articles', () =>
+  queryContent().find()  // ← まずは空で試す
+)
+
+console.log(articles.value)
+</script>
