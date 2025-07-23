6a70290cfa51e7005d377a01552617b1e84f3a64
kgwny <210300265+kgwny@users.noreply.github.com>
2025-07-23 23:49:23 +0900

    add AppHeader, AppFooter components

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    <p class="text-center">© 2025 hogehoge blog</p>
+  </footer>
+</template>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <NuxtLink to="/">
+      hogehoge blog
+    </NuxtLink>
+    <ul>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/profile">Profile</NuxtLink>
+      </li>
+    </ul>
+  </header>
+</template>
