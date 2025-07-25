0cdc3ecdd1174dea327535df15a660da183333d1
kgwny <210300265+kgwny@users.noreply.github.com>
2025-07-25 23:31:35 +0900

    update package.json and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -8,4 +10,30 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-04-03',
+  typescript: {
+    shim: false,
+  },
+  ssr: false,
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "mdi/css/materialdesignicons.min.css",
+    "vuetify/styles"
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  plugins: [
+    './plugins/vuetify.ts',
+  ],
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+    plugins: [vuetify()],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
